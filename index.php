8a445fca9abe9d74d7cdcc6e5794b0acfa613d46
JASON <tomgreat93@gmial.com>
2017-05-25 21:39:27 +0800

    0525

--- index.php
+++ index.php
@@ -1,32 +1,23 @@
-<?php
-include 'common.php';
-	session_start();        
-	if($_SESSION['username']!=null){
-		// 插入具体的用户信息在nav
-		 
-		 
-		 
-		 
-	}
 
 
-	
-
-
-// js检验用户提交表单是否为空
-
-
-// 新用户查询
-// 
-	select($link,'username','bbs_userdata','','','order by desc','');
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+	include './view/header.html';	
+	include './view/header.html';	
+	include './view/search.html';	
+	include './view/main.html';	
+	include './view/footer.html';	
 
 
 
 
-	include 'index.html';
 
 
 
 
-	?>
+
+
+
+
+
+
+
+?>
